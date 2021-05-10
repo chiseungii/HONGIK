4a304b6d281fe26c937f910ed82c728489e70bc9
tube <43616130+chiseungii@users.noreply.github.com>
2021-05-10 22:43:10 +0900

    Create hw10.c

---
+++ 1_2/C-프로그래밍/실습및과제/hw10/hw10.c
@@ -0,0 +1,20 @@
+/*
+ a++, ++a 차이 확인
+
+ a=1, b=1; 한 후
+ printf("%d %d\n", ++a, b++);
+ 해보기
+ */
+
+#include <stdio.h>
+
+int main()
+
+{
+
+	int a, b;
+	a = 1, b = 1;
+
+	printf("%d %d\n", ++a, b++);
+
+}
