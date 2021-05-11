406f619fb16b67e5435c232550fee4c7ce9f2d78
tube <43616130+chiseungii@users.noreply.github.com>
2021-05-12 04:44:03 +0900

    Create hw53.c

---
+++ 1_2/C-프로그래밍/실습및과제/hw53/hw53.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void mystrcat(char a[], char b[])
+{
+	int i, j;
+
+	for (i = 0; a[i]; i++);
+	for (j = 0; b[j]; j++, i++)
+		a[i] = b[j];
+	a[i] = 0;
+}
+
+void main()
+{
+	char str1[] = "first-", str2[] = "second";
+
+	mystrcat(str1, str2);
+
+	printf("%s\n", str1);
+}
