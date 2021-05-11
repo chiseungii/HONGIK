55c8d89c5f3792bd960fa7e0fcacc2e346f74581
tube <43616130+chiseungii@users.noreply.github.com>
2021-05-11 18:19:04 +0900

    Create hw14.c

---
+++ 1_2/C-프로그래밍/실습및과제/hw14/hw14.c
@@ -0,0 +1,26 @@
+/*
+ n! 계산
+ for 및 while 사용 2번 구현
+ */
+
+#include <stdio.h>
+
+int main()
+
+{
+
+	int num, sum;
+	sum = 1;
+
+	printf("정수를 입력하시오. : ");
+	scanf("%d", &num);
+
+	while (num > 0)
+	{
+		sum = sum * num;
+		num--;
+	}
+
+	printf("n!=%d\n", sum);
+
+}
