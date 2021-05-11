27cca1e418e9b637113c0eef9049212b47a5d951
tube <43616130+chiseungii@users.noreply.github.com>
2021-05-11 18:30:05 +0900

    Create hw17.c

---
+++ 1_2/C-프로그래밍/실습및과제/hw17/hw17.c
@@ -0,0 +1,21 @@
+/*
+ continue, break 연습
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	int i, j;
+
+	for (i = 0; i <= 4; ++i)
+	{
+		if (!(i - 2)) i = 3;
+		for (j = 3; j < 6; j = j + 2)
+		{
+			if (j == 4) continue;
+			if (j > 4) break;
+			printf("%d %d\n", i, j);
+		}
+	}
+}
