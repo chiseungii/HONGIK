76ff92a5dfcd6bfb0fe078033cbe5d2c4129f29f
tube <43616130+chiseungii@users.noreply.github.com>
2021-05-18 17:49:59 +0900

    Create Complex.cpp

---
+++ 2_1/객체지향프로그래밍/실습/14주차/lab9/Complex.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "Complex.h"
+using namespace std;
+
+// 생성자
+Complex::Complex(double a) {
+	real = a;
+	img = 0;
+}
+
+Complex::Complex(double a, double b) {
+	real = a;
+	img = b;
+}
+
+// 멤버 함수
+void Complex::print() {
+	cout << real << " + " << img << "i\n";
+}
+
+
+/*Complex Complex::add(double a) {
+	real += a;
+	return *this;
+}*/
+
+//Complex Complex::add(double a, double b = 0.0) {
+//	real += a;
+//	img += b;
+//	return *this;
+//}
+
+//Complex Complex::add(Complex c) {
+//	real += c.getReal();
+//	img += c.getImg();
+//	return *this;
+//}
+
+// 연산자 오버로딩
+ostream &operator << (ostream &os, Complex &c) {
+	os << c.getReal() << " + " << c.getImg() << "i";
+	return os;
+}
