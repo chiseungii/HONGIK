45f536ef8e31ab19fc29906852e92089314c8d10
tube <43616130+chiseungii@users.noreply.github.com>
2021-05-20 22:07:34 +0900

    Update 1.cpp

--- 2_1/객체지향프로그래밍/과제/hw4/1.cpp
+++ 2_1/객체지향프로그래밍/과제/hw4/1.cpp
@@ -3,6 +3,8 @@
  #1
  */
 
+// #define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
 #include "MyString.h"
 using namespace std;
@@ -11,8 +13,8 @@ int main() {
 	MyString my_str1, my_str2;
 
 	my_str1 = "hello world!";
-	cout << my_str1 << endl;
-	cout << my_str1.getLen() << endl;
+	cout << my_str1 << endl;	// my_str1 문자열 출력
+	cout << my_str1.getLen() << endl;	// my_str1 문자열 길이 출력
 
 	my_str2 = my_str1;
 	cout << my_str2 << endl;
