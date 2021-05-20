5bcaa062eb4c14866c19450c09a2c79321f358ac
tube <43616130+chiseungii@users.noreply.github.com>
2021-05-20 18:44:14 +0900

    Create 2.cpp

---
+++ 2_1/객체지향프로그래밍/과제/hw4/2.cpp
@@ -0,0 +1,35 @@
+/*
+ hw4
+ #2
+ */
+
+#include <iostream>
+#include "Score.h"
+using namespace std;
+
+int main() {
+	// (1)
+	Score *score_list = new Score[5];
+
+	// 각 배열 정보 입력
+	score_list[0] = Score(41, 85, 72, 38);
+	score_list[1] = Score(80, 69, 65, 68);
+	score_list[2] = Score(96, 22, 49, 67);
+	score_list[3] = Score(51, 61, 63, 87);
+	score_list[4] = Score(66, 24, 80, 83);
+
+	cout << "수학\t언어\t외국어\t사회\n";
+	for (int i = 0; i < 5; i++)
+		cout << score_list[i] << endl;
+
+	// (2)
+	cout << "평균" << endl;
+
+	Score avg = getAverageScore_1(score_list, 5);
+	cout << avg << endl;
+	avg = getAverageScore_2(score_list, 5);
+	cout << avg << endl;
+
+	// 메모리 반환
+	delete[] score_list;
+}
