91eb040eabdb760f07fa87637c49e20d7f42d179
tube <43616130+chiseungii@users.noreply.github.com>
2021-05-20 22:47:14 +0900

    Update 2.cpp

--- 2_1/객체지향프로그래밍/과제/hw4/2.cpp
+++ 2_1/객체지향프로그래밍/과제/hw4/2.cpp
@@ -3,13 +3,35 @@
  #2
  */
 
+// #define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
 #include "Score.h"
 using namespace std;
 
+Score getAverageScore_1(Score* sc, int n) {
+	Score res(0, 0, 0, 0);
+
+	for (int i = 0; i < n; i++)
+		res = res + sc[i];
+	res = res / n;
+
+	return res;
+}
+
+Score getAverageScore_2(Score* sc, int n) {
+	Score res(0, 0, 0, 0);
+
+	for (int i = 0; i < n; i++)
+		res += sc[i];
+	res /= n;
+
+	return res;
+}
+
 int main() {
 	// (1)
-	Score *score_list = new Score[5];
+	Score* score_list = new Score[5];
 
 	// 각 배열 정보 입력
 	score_list[0] = Score(41, 85, 72, 38);
@@ -19,14 +41,15 @@ int main() {
 	score_list[4] = Score(66, 24, 80, 83);
 
 	cout << "수학\t언어\t외국어\t사회\n";
-	for (int i = 0; i < 5; i++)
-		cout << score_list[i] << endl;
+	for (int i = 0; i < 5; i++) cout << score_list[i] << endl;
 
 	// (2)
 	cout << "평균" << endl;
 
-	Score avg = getAverageScore_1(score_list, 5);
+	Score avg;
+	avg = getAverageScore_1(score_list, 5);
 	cout << avg << endl;
+
 	avg = getAverageScore_2(score_list, 5);
 	cout << avg << endl;
 
