ed797f62e0d94f0d2b64c80a86cac731bd717fdd
tube <43616130+chiseungii@users.noreply.github.com>
2021-05-20 22:08:10 +0900

    Update MyString.h

--- 2_1/객체지향프로그래밍/과제/hw4/MyString.h
+++ 2_1/객체지향프로그래밍/과제/hw4/MyString.h
@@ -6,22 +6,24 @@ using namespace std;
 
 class MyString {
 private:
-	char *str;	// 문자열
+	char* str;	// 문자열
 	int str_len;	// 문자열 길이
 
 public:
 	// 생성자 & 소멸자
 	MyString();
+	MyString(char*);
+	MyString(MyString&);
 	~MyString();
 
 	// get
-	char *getString() { return str; }	// 문자열 반환
+	char* getString() { return str; }	// 문자열 반환
 	int getLen() { return str_len; }	// 문자열 길이 반환
 
 	// 연산자 오버로딩
-	void operator = (const char[]);	// 문자열을 직접 복사하기 위한 연산자
+	void operator = (char*);	// 문자열을 직접 복사하기 위한 연산자
 	void operator = (MyString);	// 다른 객체의 문자열을 복사하기 위한 연산자
-	friend ostream &operator << (ostream&, MyString&);	// 문자열 출력을 위한 연산자
+	friend ostream& operator << (ostream&, MyString&);
 };
 
 #endif
