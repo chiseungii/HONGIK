2571b0d5d9195c9dc9b84f7d578906e7d0cf80a3
tube <43616130+chiseungii@users.noreply.github.com>
2021-05-21 21:00:37 +0900

    Update RationalNumber.cpp

--- 2_1/객체지향프로그래밍/과제/hw5/RationalNumber.cpp
+++ 2_1/객체지향프로그래밍/과제/hw5/RationalNumber.cpp
@@ -1,10 +1,10 @@
 #include "RationalNumber.h"
 
 void RationalNumber::reduction() {
-	for (int i = 2; i <= numerator && i <= denominator; i++) {
+	for (int i = 2; i <= k && i <= denominator; i++) {
 		// 1로 나누는 건 무의미하므로 2부터 분자, 분모 중 작은 수까지 반복
-		if (numerator % i == 0 && denominator % i == 0) {
-			numerator /= i;
+		if (k % i == 0 && denominator % i == 0) {
+			k /= i;
 			denominator /= i;
 			i = 1;
 			// 분자와 분모 모두 나뉘어지는 i가 있으면
@@ -14,14 +14,13 @@ void RationalNumber::reduction() {
 }
 
 // 생성자
-RationalNumber::RationalNumber(int k, int m) {
-	numerator = k;
+RationalNumber::RationalNumber(int k, int m) : IntNumber(k) {
 	denominator = m;
-	reduction();	// 생성 시에도 약분 필요
+	reduction();
 }
 
 RationalNumber::RationalNumber(RationalNumber& r) {
-	numerator = r.numerator;
+	k = r.k;
 	denominator = r.denominator;
 }
 
@@ -29,16 +28,20 @@ RationalNumber::RationalNumber(RationalNumber& r) {
 RationalNumber RationalNumber::operator + (const RationalNumber& r) {
 	RationalNumber tmp;	// 결과 저장할 객체 생성
 
+	// 둘 다 정수일 때
+	if (denominator == 1 && r.denominator == 1) {
+		return RationalNumber(k + r.k);
+	}
 	// 두 객체의 분모가 같으면 분자만 더해서 약분
 	if (denominator == r.denominator) {
-		tmp.numerator = numerator + r.numerator;
+		tmp.k = k + r.k;
 		tmp.denominator = denominator;
 		tmp.reduction();
 	}
 	// 아니면 두 객체의 분모를 곱해 통분해서 더한 후 약분
 	else {
 		tmp.denominator = denominator * r.denominator;
-		tmp.numerator = numerator * r.denominator + denominator * r.numerator;
+		tmp.k = k * r.denominator + denominator * r.k;
 		tmp.reduction();
 	}
 
@@ -48,16 +51,20 @@ RationalNumber RationalNumber::operator + (const RationalNumber& r) {
 RationalNumber RationalNumber::operator - (const RationalNumber& r) {
 	RationalNumber tmp;	// 결과 저장할 객체 생성
 
-	// 두 객체의 분모가 같으면 분자만 빼서 약분
+	// 둘 다 정수일 때
+	if (denominator == 1 && r.denominator == 1) {
+		return RationalNumber(k - r.k);
+	}
+	// 두 객체의 분모가 같으면 분자만 더해서 약분
 	if (denominator == r.denominator) {
-		tmp.numerator = numerator - r.numerator;
+		tmp.k = k - r.k;
 		tmp.denominator = denominator;
 		tmp.reduction();
 	}
-	// 아니면 두 객체의 분모를 곱해 통분해서 뺀 후 약분
+	// 아니면 두 객체의 분모를 곱해 통분해서 더한 후 약분
 	else {
 		tmp.denominator = denominator * r.denominator;
-		tmp.numerator = numerator * r.denominator - denominator * r.numerator;
+		tmp.k = k * r.denominator - denominator * r.k;
 		tmp.reduction();
 	}
 
@@ -67,8 +74,13 @@ RationalNumber RationalNumber::operator - (const RationalNumber& r) {
 RationalNumber RationalNumber::operator * (const RationalNumber& r) {
 	RationalNumber tmp;
 
+	// 둘 다 정수일 때
+	if (denominator == 1 && r.denominator == 1) {
+		return RationalNumber(k * r.k);
+	}
+
 	// 두 객체의 분자와 분모를 각각 곱해줌
-	tmp.numerator = numerator * r.numerator;
+	tmp.k = k * r.k;
 	tmp.denominator = denominator * r.denominator;
 	tmp.reduction();
 
@@ -78,43 +90,48 @@ RationalNumber RationalNumber::operator * (const RationalNumber& r) {
 RationalNumber RationalNumber::operator / (const RationalNumber& r) {
 	RationalNumber tmp;
 
+	// 둘 다 정수일 때
+	if (denominator == 1 && r.denominator == 1) {
+		return RationalNumber(k / r.k);
+	}
+
 	// r 객체의 분자, 분모를 반대로 곱해줌
-	tmp.numerator = numerator * r.denominator;
-	tmp.denominator = denominator * r.numerator;
+	tmp.k = k * r.denominator;
+	tmp.denominator = denominator * r.k;
 	tmp.reduction();
 
 	return tmp;
 }
 
 bool RationalNumber::operator > (const RationalNumber& r) const {
-	return numerator * r.denominator > r.numerator * denominator;
+	return k * r.denominator > r.k * denominator;
 }
 
 bool RationalNumber::operator < (const RationalNumber& r) const {
-	return numerator * r.denominator < r.numerator * denominator;
+	return k * r.denominator < r.k * denominator;
 }
 
 bool RationalNumber::operator <= (const RationalNumber& r) const {
-	return numerator * r.denominator <= r.numerator * denominator;
+	return k * r.denominator <= r.k * denominator;
 }
 
 bool RationalNumber::operator >= (const RationalNumber& r) const {
-	return numerator * r.denominator >= r.numerator * denominator;
+	return k * r.denominator >= r.k * denominator;
 }
 
 bool RationalNumber::operator == (const RationalNumber& r) const {
-	return numerator * r.denominator == r.numerator * denominator;
+	return k * r.denominator == r.k * denominator;
 }
 
 bool RationalNumber::operator != (const RationalNumber& r) const {
-	return numerator * r.denominator != r.numerator * denominator;
+	return k * r.denominator != r.k * denominator;
 }
 
 ostream& operator << (ostream& os, const RationalNumber& r) {
 	// 분모가 1일 경우 분수 형식이 아닌 정수 형태로 분자만 출력
-	if (r.denominator == 1) os << r.numerator;
+	if (r.denominator == 1) os << r.k;
 	// 분모가 1이 아닐 경우 분수 형식으로 '/'를 추가해 출력
-	else os << r.numerator << '/' << r.denominator;
+	else os << r.k << '/' << r.denominator;
 
 	return os;
 }
